ab2796300f3e327585a66165091b03b3abb1269b
mahun <mcintyre.mlm@gmail.com>
2017-11-08 20:47:49 -0500

    tech styling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,7 +20,7 @@ function registerEventListeners(){
   var hamburger = document.getElementById('hamburger');
 
   hamburger.addEventListener('click', function(e){
-    var mobileNavContainer = document.getElementById('mobileNavContainer');
+    var mobileNavContainer = document.getElementById('mobile-nav-container');
     console.log(parseInt(mobileNavContainer.style.height));
     if(parseInt(mobileNavContainer.style.height == 0) || !parseInt(mobileNavContainer.style.height))
       mobileNavContainer.style.height = '70px';
