188e66c4323661ef75006d1cef272cdc50d0fb35
mahun <mcintyre.mlm@gmail.com>
2017-11-09 20:13:09 -0500

    remove jquer refs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,3 @@
-function initSliderOne(){
-  $('.slider-for').slick({
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   arrows: false,
-   fade: true,
-   asNavFor: '.slider-nav'
- });
- $('.slider-nav').slick({
-   slidesToShow: 3,
-   slidesToScroll: 1,
-   asNavFor: '.slider-for',
-   dots: true,
-   centerMode: true,
-   focusOnSelect: true
- });
-}
-
 function registerEventListeners(){
   var hamburger = document.getElementById('hamburger');
 
@@ -29,6 +11,6 @@ function registerEventListeners(){
   })
 }
 
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function() {
   registerEventListeners();
 });
