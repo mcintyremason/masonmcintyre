a4bf058b62cfb089d5dfeaa22bfa5347af6101f5
mcintyremason <mcintyre.mlm@gmail.com>
2018-01-05 08:22:04 -0500

    crypget router & view

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var projects = require('./routes/projects');
+var crypget = require('./routes/crypget');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', index);
 app.use('/projects', projects);
+app.use('/crypget', crypget);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/crypget.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const util= require('util');
+const fs = require('fs');
+global.fetch = require('node-fetch')
+const cc = require('cryptocompare')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('crypget/index', {
+    title: 'Mason McIntyre',
+    styles: ``,
+    scripts: ``,
+   });
+});
+
+router.post('/from-file', function(req, res, next) {
+  util.log(req);
+  res.render('crypget/index', {
+    title: 'Mason McIntyre',
+    styles: ``,
+    scripts: ``,
+   });
+});
+
+module.exports = router;
