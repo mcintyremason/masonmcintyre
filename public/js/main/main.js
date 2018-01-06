19d6403d26b25ce0b189aa472fc8b865ec471500
mcintyremason <mcintyre.mlm@gmail.com>
2018-01-06 09:23:50 -0500

    remove console logs

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -1,22 +1,18 @@
 function registerEventListeners(){
-  var hamburger = document.getElementById('hamburger');
-  var lastWindowWidth = window.innerWidth;
-  var headerContainer = document.getElementById('header-container'),
-      mobileNavContainer = document.getElementById('mobile-nav-container');
+  var hamburger = document.getElementById('hamburger'),
+    lastWindowWidth = window.innerWidth,
+    headerContainer = document.getElementById('header-container'),
+    mobileNavContainer = document.getElementById('mobile-nav-container');
 
   window.addEventListener('resize', function(e){
     if(lastWindowWidth < 768 && window.innerWidth >= 768){
-      console.log('hit');
       headerContainer.style.height = '';
       mobileNavContainer.style.height = '0px';
     }
-    
     lastWindowWidth = window.innerWidth; 
   });
 
   hamburger.addEventListener('click', function(e){
-    console.log(parseInt(mobileNavContainer.style.height));
-    console.log(headerContainer.style.height);
     if(parseInt(mobileNavContainer.style.height == 0) || !parseInt(mobileNavContainer.style.height)){
       headerContainer.style.height = '119px';
       mobileNavContainer.style.height = '45px';
