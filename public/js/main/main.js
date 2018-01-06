ed66586f9c58fd6785ed265a5d99d91424ad2701
mcintyremason <mcintyre.mlm@gmail.com>
2018-01-06 09:18:39 -0500

    fix header bugs

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -1,9 +1,20 @@
 function registerEventListeners(){
   var hamburger = document.getElementById('hamburger');
+  var lastWindowWidth = window.innerWidth;
+  var headerContainer = document.getElementById('header-container'),
+      mobileNavContainer = document.getElementById('mobile-nav-container');
+
+  window.addEventListener('resize', function(e){
+    if(lastWindowWidth < 768 && window.innerWidth >= 768){
+      console.log('hit');
+      headerContainer.style.height = '';
+      mobileNavContainer.style.height = '0px';
+    }
+    
+    lastWindowWidth = window.innerWidth; 
+  });
 
   hamburger.addEventListener('click', function(e){
-    var headerContainer = document.getElementById('header-container'),
-      mobileNavContainer = document.getElementById('mobile-nav-container');
     console.log(parseInt(mobileNavContainer.style.height));
     console.log(headerContainer.style.height);
     if(parseInt(mobileNavContainer.style.height == 0) || !parseInt(mobileNavContainer.style.height)){
